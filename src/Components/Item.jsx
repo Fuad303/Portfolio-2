d0e2a5985fbbd57489ab4d20002586ec8b50a48d
Fuad303 <sadiqovfuad8@gmail.com>
2023-01-13 20:56:53 +0400

    some possibly meaningful message

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -4,10 +4,9 @@ export default function Service(props) {
   return (
     <div className='item'>
         <a href={props.socialLink} className="item-img">
-            <div>{!props.socialIcon === '' ? <ion-icon name={props.socialIcon}></ion-icon> : <img className='logoImg' alt='item-img' src={props.socialImg}/>}</div>
+            <div>{props.socialIcon ? <ion-icon name={props.socialIcon}></ion-icon> : <img className='logoImg' alt='item-img' src={props.socialImg}/>}</div>
                 <div>
                     <span>{props.socialMedia}</span>
-                    {props.socialLink === '' ? <ion-icon name="arrow-up-outline"></ion-icon> : ""}
                 </div>
         </a>
         <div className="item-title"><span className='item-name'>{props.socialName}</span> <span className='item-date'>{props.socialDate}</span></div>
