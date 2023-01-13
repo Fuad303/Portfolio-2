af2c1f6b4e3c27f7ff5671b634557ed3e41cf697
Fuad303 <sadiqovfuad8@gmail.com>
2023-01-13 12:30:23 +0400

    Carousel added

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
     <div className="App">
       <div className='container'>
         <Navbar/>
-        {/* <Carousel/> */}
         <Hero/>
         <Services title="My skills" />
         <About/>
         <SocialMedia title="Take a look at my resume"/>
         <Articles/>
+        <Carousel/>
       </div>
     </div>
   )
--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,19 +1,97 @@
-// import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import memeImg from '../images/meme-generator.png'
+import wasteImg from '../images/waste-money.png'
+import encryptionImg from '../images/encryption.png'
+import dentistImg from '../images/dentist.png'
+import heroImg from '../images/hero-img.svg'
 
-// import 'swiper/css';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
-// export default function Carousel(){
-//   return (
-//     <Swiper
-//       spaceBetween={50}
-//       slidesPerView={3}
-//       onSlideChange={() => console.log('slide change')}
-//       onSwiper={(swiper) => console.log(swiper)}
-//     >
-//       <SwiperSlide>Slide 1</SwiperSlide>
-//       <SwiperSlide>Slide 2</SwiperSlide>
-//       <SwiperSlide>Slide 3</SwiperSlide>
-//       <SwiperSlide>Slide 4</SwiperSlide>
-//     </Swiper>
-//   );
-// };
+export default () => {
+  return (
+    <>
+    <div className="swiper-title">
+      Take a look at my design portfolio
+      <img src={heroImg} alt="" />
+    </div>
+    <Swiper
+      // install Swiper modules
+      
+      modules={[Navigation, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      breakpoints={{
+        // when window width is <= 100px
+        100: {
+          slidesPerView: 1
+        },
+        // when window width is <= 991px
+        991: {
+          slidesPerView: 2
+        },
+        // when window width is <= 1200px
+        1200: {
+          slidesPerView: 3
+        }
+      }}
+    >
+      <SwiperSlide className="swiper-item">
+        <div className="item-img">
+            <img src={memeImg} alt="meme-img" />
+        </div>
+        <div className="carousel-title">Meme generator</div>
+        <div className="carousel-text">
+          A meme generator website allows users to create 
+          and share their own memes by providing a library of 
+          popular meme templates, along with the ability 
+          to add custom text and images.
+        </div>
+        <a href='https://random-meme.netlify.app' className="carousel-link">Get to details <ion-icon name="arrow-forward-outline"></ion-icon></a>
+      </SwiperSlide>
+      <SwiperSlide className="swiper-item">
+        <div className="item-img">
+            <img src={wasteImg} alt="waste-money-img" />
+        </div>
+        <div className="carousel-title">Waste Money</div>
+        <div className="carousel-text">
+          A simple waste money website would allow users to 
+          input their spending habits and budget, then 
+          provide suggestions on where they can cut back 
+          or optimize their spending in order to save money.
+        </div>
+        <a href='https://waste-money.netlify.app' className="carousel-link">Get to details <ion-icon name="arrow-forward-outline"></ion-icon></a>
+      </SwiperSlide>
+      <SwiperSlide className="swiper-item">
+        <div className="item-img">
+            <img src={encryptionImg} alt="encryption-img" />
+        </div>
+        <div className="carousel-title">Encryption System</div>
+        <div className="carousel-text">
+          An encryption website that supports base-32, 
+          Caesar, and reverse encryption methods would 
+          allow users to securely encode their text 
+          messages or files using a variety of different 
+          techniques. 
+        </div>
+        <a href='https://encrpytion.cf' className="carousel-link">Get to details <ion-icon name="arrow-forward-outline"></ion-icon></a>
+      </SwiperSlide>
+      <SwiperSlide className="swiper-item">
+        <div className="item-img">
+            <img src={dentistImg} alt="dentist-img" />
+        </div>
+        <div className="carousel-title">Dentist Website</div>
+        <div className="carousel-text">
+          A dentist website is a website that provides 
+          information about a dental practice and 
+          the services they offer. 
+        </div>
+        <a href='https://saaf.netlify.app' className="carousel-link">Get to details <ion-icon name="arrow-forward-outline"></ion-icon></a>
+      </SwiperSlide>
+    </Swiper>
+    </>
+  );
+};
