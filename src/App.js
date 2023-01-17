839fd0b608bdeb1e013f536c344921475328c6bc
Fuad303 <sadiqovfuad8@gmail.com>
2023-01-17 13:13:31 +0400

    some update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import '../src/reset.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import SocialMedia from './Components/SocialMedia'
